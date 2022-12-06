c034406965cdfcbdec304cde90c9f5afddd04cba
Mike Johnston <mrbean355@gmail.com>
2022-12-06 19:33:34 +0200

    Solve day 6 part 2

--- src/test/kotlin/com/github/mrbean355/aoc/day6/Day6Test.kt
+++ src/test/kotlin/com/github/mrbean355/aoc/day6/Day6Test.kt
@@ -13,7 +13,13 @@ class Day6Test : PuzzleTest(Day6::class) {
         "day6/puzzle.txt" to 1707,
     )
 
-    override val part2TestCases: Map<String, Any>
-        get() = emptyMap()
+    override val part2TestCases = mapOf(
+        "day6/example-1.txt" to 19,
+        "day6/example-2.txt" to 23,
+        "day6/example-3.txt" to 23,
+        "day6/example-4.txt" to 29,
+        "day6/example-5.txt" to 26,
+        "day6/puzzle.txt" to 3697,
+    )
 
 }
